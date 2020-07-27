ab86e1a8dde027fe28092222d609ec5d79bd1ab7
Shyrik <r.wesenberg@gmail.com>
2020-07-27 17:15:27 +0200

    renderer registration

---
+++ src/main/java/dev/shyrik/modularitemframe/ModularItemFrameClient.java
@@ -0,0 +1,14 @@
+package dev.shyrik.modularitemframe;
+
+import dev.shyrik.modularitemframe.client.FrameRenderer;
+import dev.shyrik.modularitemframe.init.Registrar;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+
+public class ModularItemFrameClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Here we will put client-only registration code
+        BlockEntityRendererRegistry.INSTANCE.register(Registrar.MODULARFRAME_ENTITY, FrameRenderer::new);
+    }
+}
