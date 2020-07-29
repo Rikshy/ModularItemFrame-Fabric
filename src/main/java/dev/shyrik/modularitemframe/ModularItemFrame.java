1f71a4251ee4c794646836fb22259042d2d2634b
Shyrik <r.wesenberg@gmail.com>
2020-07-29 22:31:36 +0200

    fixed crash on startup

--- src/main/java/dev/shyrik/modularitemframe/ModularItemFrame.java
+++ src/main/java/dev/shyrik/modularitemframe/ModularItemFrame.java
@@ -1,5 +1,6 @@
 package dev.shyrik.modularitemframe;
 
+import dev.shyrik.modularitemframe.api.util.RegistryHelper;
 import dev.shyrik.modularitemframe.init.ModularItemFrameConfig;
 import dev.shyrik.modularitemframe.init.Registrar;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
@@ -15,7 +16,7 @@ public class ModularItemFrame implements ModInitializer {
     public static ItemGroup GROUP = new ItemGroup(1, MOD_ID) {
         @Override
         public ItemStack createIcon() {
-            return null;
+            return new ItemStack(Registrar.SCREWDRIVER);
         }
     };
 
