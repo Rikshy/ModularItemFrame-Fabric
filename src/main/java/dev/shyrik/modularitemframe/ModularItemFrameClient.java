9d059a6b723750d69fa010de1ac67105f0077fb4
Shyrik <r.wesenberg@gmail.com>
2020-08-01 03:05:22 +0200

    don't do mixin where mixin already is (stitching)
    i was so proud to get it done by myself :(

--- src/main/java/dev/shyrik/modularitemframe/ModularItemFrameClient.java
+++ src/main/java/dev/shyrik/modularitemframe/ModularItemFrameClient.java
@@ -1,18 +1,54 @@
 package dev.shyrik.modularitemframe;
 
 import dev.shyrik.modularitemframe.api.mixin.ApplyModelLoaderCallback;
-import dev.shyrik.modularitemframe.api.mixin.PreStitchCallback;
 import dev.shyrik.modularitemframe.client.FrameRenderer;
-import dev.shyrik.modularitemframe.init.ClientSetup;
+import dev.shyrik.modularitemframe.common.block.ModularFrameBlock;
+import dev.shyrik.modularitemframe.common.module.t1.*;
+import dev.shyrik.modularitemframe.common.module.t2.*;
+import dev.shyrik.modularitemframe.common.module.t3.*;
 import dev.shyrik.modularitemframe.init.Registrar;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+
+import java.util.Arrays;
 
 public class ModularItemFrameClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        PreStitchCallback.EVENT.register(ClientSetup::stitch);
+        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX)
+                .register((spriteAtlasTexture, registry) -> stitch(registry));
         ApplyModelLoaderCallback.EVENT.register(FrameRenderer::onApplyModelLoader);
         BlockEntityRendererRegistry.INSTANCE.register(Registrar.MODULAR_FRAME_ENTITY, FrameRenderer::new);
+    }
+
+    public void stitch(ClientSpriteRegistryCallback.Registry registry) {
+        Arrays.asList(
+                ModularFrameBlock.INNER_HARDEST_LOC,
+                ModularFrameBlock.INNER_HARD_LOC,
+
+                CraftingModule.BG_LOC,
+                IOModule.BG_LOC,
+                ItemModule.BG_LOC,
+                NullifyModule.BG_LOC,
+                StorageModule.BG_LOC,
+                TankModule.BG_LOC,
+
+                CraftingPlusModule.BG_LOC,
+                DispenseModule.BG_LOC,
+                TrashCanModule.BG_LOC1,
+                TrashCanModule.BG_LOC2,
+                TrashCanModule.BG_LOC3,
+                UseModule.BG_LOC,
+                VacuumModule.BG_LOC,
+
+                AutoCraftingModule.BG_LOC,
+                FluidDispenserModule.BG_LOC,
+                ItemTeleportModule.BG_IN,
+                ItemTeleportModule.BG_OUT,
+                ItemTeleportModule.BG_NONE,
+                XPModule.BG_LOC
+        ).forEach(registry::register);
     }
 }
