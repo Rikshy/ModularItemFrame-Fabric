a3d3814c5bce31af8a5999d49a85b5bb88fbce40
Shyrik <r.wesenberg@gmail.com>
2020-08-31 10:25:55 +0200

    java docs for upgrades

--- src/main/java/dev/shyrik/modularitemframe/api/UpgradeBase.java
+++ src/main/java/dev/shyrik/modularitemframe/api/UpgradeBase.java
@@ -18,21 +18,45 @@ public abstract class UpgradeBase {
         return item;
     }
 
+    /**
+     * @return max times this upgrade can be present in a frame.
+     */
     public abstract int getMaxCount();
+
+    /**
+     * @return unique ID the upgrade gets registered with.
+     */
     public abstract Identifier getId();
 
+    /**
+     * @return the name of the upgrade :O
+     */
     @Environment(EnvType.CLIENT)
     public abstract Text getName();
 
+    /**
+     * Called when upgrade is added to a frame.
+     */
     public void onInsert(World world, BlockPos pos, Direction facing, PlayerEntity player, ItemStack upStack) {
     }
 
+    /**
+     * Called when upgrade is removed with the {@link dev.shyrik.modularitemframe.common.item.ScrewdriverItem}
+     * or destroyed.
+     */
     public void onRemove(World world, BlockPos pos, Direction facing, PlayerEntity player, ItemStack upStack) {
     }
 
+    /**
+     * Tag serialization in case there are some data to be saved!
+     */
     public CompoundTag toTag() {
         return new CompoundTag();
     }
+
+    /**
+     * Tag deserialization in case there are some data to be saved!
+     */
     public void fromTag(CompoundTag tag) {
     }
 }
